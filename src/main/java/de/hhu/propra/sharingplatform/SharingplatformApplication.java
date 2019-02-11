95f48aa4dfd2d65377304c8ce6c9f3c5abac25d6
Christian Laußmann <ch.laussmann@gmail.com>
2019-02-11 12:35:00 +0100

    INITIALIZE with emplty spring repository

---
+++ src/main/java/de/hhu/propra/sharingplatform/SharingplatformApplication.java
@@ -0,0 +1,14 @@
+package de.hhu.propra.sharingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharingplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharingplatformApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/de/hhu/propra/sharingplatform/SharingplatformApplicationTests.java
@@ -0,0 +1,17 @@
+package de.hhu.propra.sharingplatform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SharingplatformApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
