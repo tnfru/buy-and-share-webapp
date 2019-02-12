b30ec765b75df5694f82ff030706301fc0978272
Philipp Krause <phil.fhs11@googlemail.com>
2019-02-12 17:28:01 +0100

    ADD: user and items are saved to database

--- src/main/java/de/hhu/propra/sharingplatform/faker/DataFaker.java
+++ src/main/java/de/hhu/propra/sharingplatform/faker/DataFaker.java
@@ -3,22 +3,29 @@ package de.hhu.propra.sharingplatform.faker;
 import com.github.javafaker.Faker;
 import de.hhu.propra.sharingplatform.model.Item;
 import de.hhu.propra.sharingplatform.model.User;
+import de.hhu.propra.sharingplatform.modelDAO.ItemRepo;
+import de.hhu.propra.sharingplatform.modelDAO.UserRepo;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Random;
+import javax.servlet.ServletContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
-import org.springframework.stereotype.Component;
 import org.springframework.core.env.Environment;
-
-import javax.servlet.ServletContext;
-import java.util.Locale;
+import org.springframework.stereotype.Component;
 
 @Component
 public class DataFaker implements ServletContextInitializer {
 
     @Autowired
     private Environment env;
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private ItemRepo itemRepo;
 
     private Faker faker;
 
@@ -50,7 +57,23 @@ public class DataFaker implements ServletContextInitializer {
             User user = getRandomUser(users);
             itemFaker.createItems(items, user, 2);
         }
+
+        persistUser(users);
+        persistItem(items);
+
         System.out.println("Done!");
+    }
+
+    private void persistUser(List<User> users) {
+        for (User user : users) {
+            userRepo.save(user);
+        }
+    }
+
+    private void persistItem(List<Item> items) {
+        for (Item item : items) {
+            itemRepo.save(item);
+        }
     }
 
     private User getRandomUser(List<User> users) {
