bcf30f946464245c633a9fd29a24422dffd78671
pYTR0N <lars.lmueller@gmail.com>
2019-02-12 12:56:19 +0100

    REFACTOR: indentation

--- src/main/java/de/hhu/propra/sharingplatform/SharingplatformApplication.java
+++ src/main/java/de/hhu/propra/sharingplatform/SharingplatformApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SharingplatformApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SharingplatformApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SharingplatformApplication.class, args);
+    }
 
 }
 
