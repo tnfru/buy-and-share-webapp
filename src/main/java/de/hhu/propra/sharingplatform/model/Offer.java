942acab9bb68ef79b4288aa991380b0204b296c0
Ingo Kral <ingo.kral@hhu.de>
2019-02-12 12:25:11 +0100

    ADD: Offer and OfferService

---
+++ src/main/java/de/hhu/propra/sharingplatform/model/Offer.java
@@ -0,0 +1,20 @@
+package de.hhu.propra.sharingplatform.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class Offer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private Item item;
+    private User wantsToBorrow;
+    private boolean accept = false;
+}
