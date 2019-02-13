72f5dc4c00c92935d6678b4ea3211d3674f12088
Christian Laußmann <ch.laussmann@gmail.com>
2019-02-13 12:28:18 +0100

    REFACTOR: Controller, split into responsibilities

--- src/main/java/de/hhu/propra/sharingplatform/web_controller/SharingPlatformController.java
+++ src/main/java/de/hhu/propra/sharingplatform/web_controller/SharingPlatformController.java
@@ -1,14 +1,9 @@
 package de.hhu.propra.sharingplatform.web_controller;
 
-import de.hhu.propra.sharingplatform.form.UserForm;
 import java.io.IOException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class SharingPlatformController {
@@ -22,38 +17,4 @@ public class SharingPlatformController {
         return "mainpage";
     }
 
-    @GetMapping("/login")
-    public String loginPage(Model model) {
-        return "login";
-    }
-
-    @GetMapping("/logout")
-    public String logoutPage(Model model) {
-        return "logout";
-    }
-
-    @GetMapping("/register")
-    public String registerPage(Model model) {
-        UserForm form = new UserForm();
-        model.addAttribute("userForm", form);
-        return "register";
-    }
-
-    @PostMapping("/register")
-    public String registerNewUser(Model model, @ModelAttribute("userForm") UserForm userForm) {
-        System.out.println(userForm.parseToUser());
-        return "mainpage";
-    }
-
-    @GetMapping("/account/{id}")
-    public String accountPage(Model model, @PathVariable(value = "id", required = true) long id) {
-        return "account";
-    }
-
-    @GetMapping("/details/{id}")
-    public String detailPage(Model model,
-        @PathVariable(value = "id", required = true) long id) {
-        //TODO: add param do differentiate users
-        return "details";
-    }
 }
