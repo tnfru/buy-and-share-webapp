cd943403533bea01b42b5ecf5764407e597ae33a
Philipp Krause <phil.fhs11@googlemail.com>
2019-02-13 13:16:23 +0100

    ADD: end contract and payment creation

--- src/main/java/de/hhu/propra/sharingplatform/service/ContractService.java
+++ src/main/java/de/hhu/propra/sharingplatform/service/ContractService.java
@@ -2,7 +2,9 @@ package de.hhu.propra.sharingplatform.service;
 
 import de.hhu.propra.sharingplatform.model.Contract;
 import de.hhu.propra.sharingplatform.model.Offer;
+import de.hhu.propra.sharingplatform.model.User;
 import de.hhu.propra.sharingplatform.modelDAO.ContractRepo;
+import java.util.Date;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,8 +16,31 @@ public class ContractService {
     @Autowired
     ContractRepo contractRepo;
 
+    @Autowired
+    PaymentRepo paymentRepo;
+
     public void create(Offer offer) {
         contractRepo.save(new Contract(offer));
     }
 
+    public void endContract(long contractId) {
+        Contract contract = contractRepo.findOneById(contractId);
+        Date current = new Date();
+
+        contract.setRealEnd(current);
+        contractRepo.save(contract);
+    }
+
+    public void calcPrice(long contractId) {
+        Contract contract = contractRepo.findOneById(contractId);
+
+        long startTime = contract.getStart().getTime();
+        long endTime = contract.getRealEnd().getTime();
+
+        long timePassed = (endTime - startTime) / (1000 * 60 * 60 * 24);
+        User from = contract.getBorrower();
+        User to = contract.getItem().getOwner();
+        Payment payment = new Payment(from, to, timePassed * contract.getItem().getPrice());
+        paymentRepo.save(payment);
+    }
 }
