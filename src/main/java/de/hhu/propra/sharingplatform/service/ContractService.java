be84c04defc0be0884bb74511898e99a8b507d1e
pYTR0N <lars.lmueller@gmail.com>
2019-02-13 11:09:15 +0100

    ADD: contract / offer service template

--- src/main/java/de/hhu/propra/sharingplatform/service/ContractService.java
+++ src/main/java/de/hhu/propra/sharingplatform/service/ContractService.java
@@ -2,13 +2,20 @@ package de.hhu.propra.sharingplatform.service;
 
 import de.hhu.propra.sharingplatform.model.Contract;
 import de.hhu.propra.sharingplatform.model.Offer;
+import de.hhu.propra.sharingplatform.modelDAO.ContractRepo;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Data
 public class ContractService {
 
-    public Contract offerAccepted(Offer offer) {
-        return offer.isAccept() ? new Contract(offer) : null;
+    @Autowired
+    ContractRepo contractRepo;
+
+    public void create(Offer offer) {
+        contractRepo.save(new Contract(offer));
     }
 
 }
