4ea3d00433a32e66a90ea369654b00a97da82757
Jonathan Bobak <jonathan.bobak@yahoo.de>
2019-02-13 13:59:58 +0100

    ADD: Item service extracted from item controller

--- src/main/java/de/hhu/propra/sharingplatform/web_controller/ItemController.java
+++ src/main/java/de/hhu/propra/sharingplatform/web_controller/ItemController.java
@@ -2,16 +2,14 @@ package de.hhu.propra.sharingplatform.web_controller;
 
 import de.hhu.propra.sharingplatform.model.Item;
 import de.hhu.propra.sharingplatform.model.User;
-import de.hhu.propra.sharingplatform.modelDAO.ContractRepo;
-import de.hhu.propra.sharingplatform.modelDAO.ItemRepo;
 import de.hhu.propra.sharingplatform.modelDAO.UserRepo;
+import de.hhu.propra.sharingplatform.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import sun.security.krb5.internal.ccache.CredentialsCache;
 
 @Controller
 public class ItemController {
@@ -20,7 +18,7 @@ public class ItemController {
     private UserRepo userRepo;
 
     @Autowired
-    private ItemRepo itemRepo;
+    private ItemService itemService;
 
     @GetMapping("/item/details/{id}")
     public String detailPage(Model model,
@@ -38,9 +36,7 @@ public class ItemController {
 
     @PostMapping("/item/newItem/{userId}")
     public String inputItemData(Model model, Item item, @PathVariable long userId) {
-        User user = userRepo.findOneById(userId);
-        item.setOwner(user);
-        itemRepo.save(item);
+        itemService.persistItem(item, userId);
         return "redirect:/user/account/" + userId;
     }
 }
