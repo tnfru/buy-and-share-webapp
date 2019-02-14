e92928eaf81ca5e1f998091158486ae06bd1eeab
Philipp Krause <phil.fhs11@googlemail.com>
2019-02-14 10:34:25 +0100

    REMOVE: datajpatest which was not required for tests

--- src/test/java/de/hhu/propra/sharingplatform/service/ContractServiceTest.java
+++ src/test/java/de/hhu/propra/sharingplatform/service/ContractServiceTest.java
@@ -18,13 +18,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@DataJpaTest
 @Import({Offer.class, Contract.class, ContractService.class})
 public class ContractServiceTest {
 
