49f6a17a17530efee28181340836fb898718aeb8
Sammoer <sami.hamdan@hhu.de>
2019-02-14 15:20:53 +0100

    REFACTOR: Join PersonProvider and UserRepo

--- src/main/java/de/hhu/propra/sharingplatform/dao/UserRepo.java
+++ src/main/java/de/hhu/propra/sharingplatform/dao/UserRepo.java
@@ -3,6 +3,9 @@ package de.hhu.propra.sharingplatform.dao;
 import de.hhu.propra.sharingplatform.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends CrudRepository<User, Long> {
     User findOneById(long id);
+    Optional<User> findByEmail(String username);
 }
