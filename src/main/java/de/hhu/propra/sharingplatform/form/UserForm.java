9f9a51126f486a7f015999f578630eac69de82cd
Christian Laußmann <ch.laussmann@gmail.com>
2019-02-14 11:59:18 +0100

    ADD: Set password on registration

--- src/main/java/de/hhu/propra/sharingplatform/form/UserForm.java
+++ src/main/java/de/hhu/propra/sharingplatform/form/UserForm.java
@@ -11,35 +11,44 @@ import org.springframework.web.server.ResponseStatusException;
 
 @Data
 public class UserForm {
-    /*public UserForm() {}*/
-    /*public UserForm(String name, String address, String email, String propayId) {
-
-    }*/
 
     private String name;
     private String address;
     private String email;
     private String propayId;
+    private String password;
+    private String passwordConfirm;
 
     public User parseToUser() {
         if (!validateUserForm()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cant validate formdata!");
+        }
+        if(! password.equals(passwordConfirm)){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Passwords did not match!");
+        }
+        if(password.length() < 8){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Password is too short!");
+        }
+        if(propayId == null) {
+            propayId = "propay-" + email;
         }
         User user = new User();
         user.setName(name);
         user.setAddress(address);
         user.setEmail(email);
         user.setPropayId(propayId);
+        user.setPassword(password);
         return user;
     }
 
-    //TODO default propayid = name
     private boolean validateUserForm() {
-        if (name == null || address == null || email == null || propayId == null) return false;
+        if (name == null || address == null || email == null || password == null) {
+            return false;
+        }
         if (name.length() == 0 || name.length() > 255 || !isAlphanumeric(name)) return false;
         if (address.length() == 0 || address.length() > 255) return false;
         if (email.length() == 0 || email.length() > 255 || !validateMail()) return false;
-        if (propayId.length() == 0 || propayId.length() > 255) return false;
+        if (password.length() == 0 || password.length() > 255) return false;
         return true;
     }
 
