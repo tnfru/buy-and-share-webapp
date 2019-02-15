c617e4ebc0dc3b93d1e4f81e7e9bca0940929c9f
Jonathan Bobak <jonathan.bobak@yahoo.de>
2019-02-15 11:20:05 +0100

    REFACTOR: Rename changed import

--- src/main/java/de/hhu/propra/sharingplatform/controller/UserController.java
+++ src/main/java/de/hhu/propra/sharingplatform/controller/UserController.java
@@ -1,8 +1,6 @@
 package de.hhu.propra.sharingplatform.controller;
 
-import de.hhu.propra.sharingplatform.form.UserForm;
-import de.hhu.propra.sharingplatform.model.Item;
-import de.hhu.propra.sharingplatform.model.User;
+import de.hhu.propra.sharingplatform.dto.UserForm;
 import de.hhu.propra.sharingplatform.dao.ItemRepo;
 import de.hhu.propra.sharingplatform.dao.UserRepo;
 import java.security.Principal;
@@ -11,7 +9,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
