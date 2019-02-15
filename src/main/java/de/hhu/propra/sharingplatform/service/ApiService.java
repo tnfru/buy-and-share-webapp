472caad53dabc64d2d32099436db3d5340d76b2e
Jonathan Bobak <jonathan.bobak@yahoo.de>
2019-02-15 11:20:31 +0100

    ADD: Service to communicate with the ProPay API

--- src/main/java/de/hhu/propra/sharingplatform/service/ApiService.java
+++ src/main/java/de/hhu/propra/sharingplatform/service/ApiService.java
@@ -1,11 +1,30 @@
 package de.hhu.propra.sharingplatform.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.hhu.propra.sharingplatform.dto.ProPay;
 import lombok.Data;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 @Data
 @Service
 public class ApiService {
-    
 
+    String host = "localhost";
+
+    String fetchJson(String userName) {
+        String url = "http://" + host + ":8888/account/" + userName;
+        RestTemplate jsonResponse = new RestTemplate();
+
+        return jsonResponse.getForObject(url, String.class);
+    }
+
+    public ProPay mapJson(String userName) throws IOException {
+        String jsonResponse = fetchJson(userName);
+        ObjectMapper mapper = new ObjectMapper();
+
+        return mapper.readValue(jsonResponse, ProPay.class);
+    }
 }
