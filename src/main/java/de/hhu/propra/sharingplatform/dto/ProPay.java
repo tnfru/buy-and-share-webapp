c4ab266eaaf3c5a0c2a107e63c48f08e33744be8
Jonathan Bobak <jonathan.bobak@yahoo.de>
2019-02-15 11:19:03 +0100

    ADD: DTO for Json mapping

---
+++ src/main/java/de/hhu/propra/sharingplatform/dto/ProPay.java
@@ -0,0 +1,16 @@
+package de.hhu.propra.sharingplatform.dto;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Data
+@Component
+public class ProPay {
+
+
+    private String account;
+    private long amount;
+    List<ProPayReservation> reservations;
+}
---
+++ src/main/java/de/hhu/propra/sharingplatform/dto/ProPayReservation.java
@@ -0,0 +1,12 @@
+package de.hhu.propra.sharingplatform.dto;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class ProPayReservation {
+
+    private long amount;
+    private long id;
+}
