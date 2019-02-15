5c88ee794917c66b46d934f4010d72f9eaa9f5c5
Christian Laußmann <ch.laussmann@gmail.com>
2019-02-15 09:26:14 +0100

    REFACTOR: Codestyle

--- src/main/java/de/hhu/propra/sharingplatform/controller/SharingPlatformErrorController.java
+++ src/main/java/de/hhu/propra/sharingplatform/controller/SharingPlatformErrorController.java
@@ -1,7 +1,6 @@
 package de.hhu.propra.sharingplatform.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
-import static org.springframework.http.HttpStatus.*;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @Controller
-public class SharingPlatformErrorController  implements ErrorController {
+public class SharingPlatformErrorController implements ErrorController {
 
     @Override
     public String getErrorPath() {
@@ -21,7 +18,7 @@ public class SharingPlatformErrorController  implements ErrorController {
     }
 
     @RequestMapping("/error")
-    public String errorHandling(HttpServletRequest request, Model model){
+    public String errorHandling(HttpServletRequest request, Model model) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         Object errormsg = request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
         int statusCode = 0;
@@ -29,7 +26,7 @@ public class SharingPlatformErrorController  implements ErrorController {
             statusCode = Integer.valueOf(status.toString());
             model.addAttribute("errorcode", statusCode);
         }
-        if(errormsg != null){
+        if (errormsg != null) {
             model.addAttribute("errormsg", (String) errormsg);
         }
         return "error";
