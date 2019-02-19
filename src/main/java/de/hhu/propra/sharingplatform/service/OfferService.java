e8591fc6ba0717f1d6f23b2dc36f2221d74137e5
Jonathan Bobak <jonathan.bobak@yahoo.de>
2019-02-19 15:26:24 +0100

    ADD: decline all contracts with overlap

--- src/main/java/de/hhu/propra/sharingplatform/service/OfferService.java
+++ src/main/java/de/hhu/propra/sharingplatform/service/OfferService.java
@@ -72,19 +72,6 @@ public class OfferService {
         }
     }
 
-    void accept(long id) {
-        Offer offer = offerRepo.findOneById(id);
-        offer.setAccept(true);
-        offerRepo.save(offer);
-        contractService.create(offer);
-    }
-
-    void decline(long id) {
-        Offer offer = offerRepo.findOneById(id);
-        offer.setDecline(true);
-        offerRepo.save(offer);
-    }
-
     public List<Offer> getItemOffers(long itemId, User user) {
         if (itemService.userIsOwner(itemId, user.getId())) {
             return offerRepo.findAllByItemIdAndAcceptIsFalseAndDeclineIsFalse(itemId);
@@ -99,12 +86,32 @@ public class OfferService {
         Offer offer = offerRepo.findOneById(offerId);
         if (itemService.userIsOwner(offer.getItem().getId(), user.getId())) {
             offer.setAccept(true);
+            removeOverlappingOffer(offer);
             offerRepo.save(offer);
             //TODO: create contract needs ProPay Api
             // contractService.create(offer);
         } else {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN,
                 "This item does not belong to you");
+        }
+    }
+
+    private void removeOverlappingOffer(Offer offer) {
+        Item item = offer.getItem();
+        List<Offer> offersWithSameItem = offerRepo.findAllByItemId(item.getId());
+        for (Offer offerToTest : offersWithSameItem ) {
+            if (offer.getId().equals(offerToTest.getId())) continue;
+            if (offer.getStart().after(offerToTest.getStart())) {
+                if (offerToTest.getEnd().after(offer.getStart())) {
+                    offerToTest.setDecline(true);
+                    offerRepo.save(offerToTest);
+                }
+            } else {
+                if (offer.getEnd().after(offerToTest.getStart())) {
+                    offerToTest.setDecline(true);
+                    offerRepo.save(offerToTest);
+                }
+            }
         }
     }
 
