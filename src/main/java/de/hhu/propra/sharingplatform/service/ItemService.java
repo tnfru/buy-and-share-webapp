927e01af35b11dfc72ada214ccc1186d637d4643
Christian Laußmann <ch.laussmann@gmail.com>
2019-02-19 09:41:34 +0100

    FIX: Removed items can no longer be displayed or edited

--- src/main/java/de/hhu/propra/sharingplatform/service/ItemService.java
+++ src/main/java/de/hhu/propra/sharingplatform/service/ItemService.java
@@ -4,7 +4,9 @@ import de.hhu.propra.sharingplatform.dao.ItemRepo;
 import de.hhu.propra.sharingplatform.dao.UserRepo;
 import de.hhu.propra.sharingplatform.model.Item;
 import de.hhu.propra.sharingplatform.model.User;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class ItemService {
@@ -42,11 +44,15 @@ public class ItemService {
     }
 
     public Item findItem(long itemId) {
-        return itemRepo.findOneById(itemId);
+        Item item = itemRepo.findOneById(itemId);
+        if(item.isDeleted()){
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "This Item was deleted");
+        }
+        return item;
     }
 
     public void editItem(Item newItem, long oldItemId, long userId) {
-        if (validateItem(newItem) && userIsOwner(itemRepo.findOneById(oldItemId), userId)) {
+        if (validateItem(newItem) && userIsOwner(findItem(oldItemId), userId)) {
             Item oldItem = itemRepo.findOneById(oldItemId);
             newItem.setOwner(oldItem.getOwner());
             newItem.setId(oldItem.getId());
