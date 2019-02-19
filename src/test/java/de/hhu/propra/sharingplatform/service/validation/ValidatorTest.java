24e61dfc0cb477f3499b839326345ba072ef830d
Christian Laußmann <ch.laussmann@gmail.com>
2019-02-19 17:20:34 +0100

    ADD: More tests for Validator

--- src/test/java/de/hhu/propra/sharingplatform/service/validation/ValidatorTest.java
+++ src/test/java/de/hhu/propra/sharingplatform/service/validation/ValidatorTest.java
@@ -7,105 +7,120 @@ import static org.junit.Assert.*;
 public class ValidatorTest {
 
     @Test
-    public void mailTestValidMail1(){
+    public void mailTestValidMail1() {
         assertTrue(Validator.isValidMail("foo@bar.com"));
     }
 
     @Test
-    public void mailTestValidMail2(){
+    public void mailTestValidMail2() {
         assertTrue(Validator.isValidMail("foo.bar@bar.com"));
     }
 
     @Test
-    public void mailTestValidMail3(){
+    public void mailTestValidMail3() {
         assertTrue(Validator.isValidMail("foo1999@bar.com"));
     }
 
     @Test
-    public void mailTestInvalidMail1(){
+    public void mailTestValidMail4() {
+        assertTrue(Validator.isValidMail("foo-bar@bar.com"));
+    }
+
+    @Test
+    public void mailTestInvalidMail1() {
         assertFalse(Validator.isValidMail("f$_@bar.com"));
     }
 
     @Test
-    public void mailTestInvalidMail2(){
+    public void mailTestInvalidMail2() {
         assertFalse(Validator.isValidMail("bar.com"));
     }
 
     @Test
-    public void mailTestInvalidMail3(){
+    public void mailTestInvalidMail3() {
         assertFalse(Validator.isValidMail("foo@bar"));
     }
 
     @Test
-    public void mailTestInvalidMail4(){
+    public void mailTestInvalidMail4() {
         assertFalse(Validator.isValidMail("@bar.com"));
     }
 
     ///////////////////////////
 
     @Test
-    public void noSpecialChars1(){
+    public void noSpecialChars1() {
         assertFalse(Validator.freeOfSpecialChars("aaa!bbb"));
     }
 
     @Test
-    public void noSpecialChars2(){
+    public void noSpecialChars2() {
         assertTrue(Validator.freeOfSpecialChars("aaa-bbb"));
     }
 
     @Test
-    public void noSpecialChars3(){
+    public void noSpecialChars3() {
         assertFalse(Validator.freeOfSpecialChars("aa%&$"));
     }
 
     @Test
-    public void noSpecialChars4(){
+    public void noSpecialChars4() {
         assertTrue(Validator.freeOfSpecialChars("aaa123bbb"));
+    }
+
+    @Test
+    public void noSpecialChars5() {
+        assertTrue(Validator.freeOfSpecialChars("b.a-c"));
+    }
+
+    @Test
+    public void noSpecialChars6() {
+        assertTrue(Validator.freeOfSpecialChars("aa bb"));
     }
 
     /////////////////////////////
 
     @Test
-    public void alphaNumericMinus(){
+    public void alphaNumericMinus() {
         assertFalse(Validator.isAlphanumeric("a-b"));
     }
 
     @Test
-    public void alphaNumericSpace(){
+    public void alphaNumericSpace() {
         assertFalse(Validator.isAlphanumeric("a b"));
     }
 
     @Test
-    public void alphaNumericLetters(){
+    public void alphaNumericLetters() {
         assertTrue(Validator.isAlphanumeric("a1b"));
     }
 
     @Test
-    public void alphaNumericNumbers(){
+    public void alphaNumericNumbers() {
         assertTrue(Validator.isAlphanumeric("123"));
     }
 
     //////////////////////////////
 
     @Test
-    public void matchesGuidlinesEmpty(){
+    public void matchesGuidlinesEmpty() {
         assertFalse(Validator.matchesDBGuidlines(""));
     }
 
     @Test
-    public void matchesGuidlinesNull(){
+    public void matchesGuidlinesNull() {
         assertFalse(Validator.matchesDBGuidlines(null));
     }
 
     @Test
-    public void matchesGuidlinesNormal(){
+    public void matchesGuidlinesNormal() {
         assertTrue(Validator.matchesDBGuidlines("!_llslfg$"));
     }
 
     @Test
-    public void matchesGuidlinesTooLong(){
+    public void matchesGuidlinesTooLong() {
         String veryLongSTring = "A";
-        for(int i=0; i< 256; i++){
+        for (int i = 0; i < 256; i++) {
             veryLongSTring += "B";
         }
         assertFalse(Validator.matchesDBGuidlines(veryLongSTring));
@@ -114,12 +129,12 @@ public class ValidatorTest {
     //////////////////////////////
 
     @Test
-    public void isPrintableTrue(){
+    public void isPrintableTrue() {
         assertTrue(Validator.isPrintable("!§$%&/()=äöüu"));
     }
 
     @Test
-    public void isPrintableFalse(){
+    public void isPrintableFalse() {
         assertFalse(Validator.isPrintable("\n"));
     }
 }
