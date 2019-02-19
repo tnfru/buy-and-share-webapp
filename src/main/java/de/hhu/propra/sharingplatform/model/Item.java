4ce4e5210ae3aea078c38642ac3540c4efd98201
Jonathan Bobak <jonathan.bobak@yahoo.de>
2019-02-19 20:56:12 +0100

    REFACTOR: checkstyle

--- src/main/java/de/hhu/propra/sharingplatform/model/Item.java
+++ src/main/java/de/hhu/propra/sharingplatform/model/Item.java
@@ -1,17 +1,12 @@
 package de.hhu.propra.sharingplatform.model;
 
 
-import java.util.ArrayList;
-import java.util.List;
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import lombok.Data;
 import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
@@ -49,7 +44,7 @@ public class Item {
     public long getActiveOffers() {
         long counter = 0;
         for (Offer offer : offers) {
-            if(!(offer.isAccept() || offer.isDecline())) {
+            if (!(offer.isAccept() || offer.isDecline())) {
                 counter++;
             }
         }
