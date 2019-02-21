a00890f4fc27b14bac177bec8db76e2af0456adc
Jonathan Bobak <jonathan.bobak@yahoo.de>
2019-02-21 11:37:24 +0100

    ADD: Contract Controller

---
+++ src/main/java/de/hhu/propra/sharingplatform/controller/ContractController.java
@@ -0,0 +1,28 @@
+package de.hhu.propra.sharingplatform.controller;
+
+import de.hhu.propra.sharingplatform.service.ContractService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.security.Principal;
+
+@Controller
+public class ContractController {
+
+    @Autowired
+    private ContractService contractService;
+
+    @PostMapping("/contract/{contractId}/acceptReturn")
+    public String acceptReturn(@PathVariable long contractId, Principal principal) {
+        contractService.acceptReturn(contractId, principal.getName());
+        return "redirect:/user/account";
+    }
+
+    @PostMapping("/contract/{contractId}/openConflict")
+    public String openConflict(@PathVariable long contractId, Principal principal) {
+        contractService.openConflict(contractId, principal.getName());
+        return "redirect:/user/account";
+    }
+}
