ff9c7933c3281f3a6471f4ec0c9d9035e4729a8d
Christian Laußmann <ch.laussmann@gmail.com>
2019-02-25 11:45:10 +0100

    ADD: Javadoc for paymant api

--- src/main/java/de/hhu/propra/sharingplatform/service/payment/IPaymentApi.java
+++ src/main/java/de/hhu/propra/sharingplatform/service/payment/IPaymentApi.java
@@ -1,25 +1,77 @@
 package de.hhu.propra.sharingplatform.service.payment;
 
-import de.hhu.propra.sharingplatform.model.Payment;
-import de.hhu.propra.sharingplatform.model.User;
 import org.springframework.stereotype.Component;
 
 @Component
 public interface IPaymentApi {
 
+    /**
+     * Reserve money on the account.
+     *
+     * @param fromAccount account from which the money is reserved.
+     * @param toAccount   account to which the money will be send
+     *                    once the reservation is punished.
+     * @param amount      amount of money reserved.
+     * @return the ID of the reservation. Use this ID for free/punishReservation().
+     */
     long reserveMoney(String fromAccount, String toAccount, int amount);
 
-    void freeReservation(long reservationID, String accountSender);
+    /**
+     * Free the reservation done by reserveMoney().
+     *
+     * @param reservationId ID as it is returned by reserveMoney().
+     * @param accountSender accountID od the sender.
+     */
+    void freeReservation(long reservationId, String accountSender);
 
-    void punishReservation(long reservation, String accountSender);
+    /**
+     * Punish the reservation done by reserveMoney(). Money is transferred.
+     *
+     * @param reservationId ID as it is returned by reserveMoney().
+     * @param accountSender accountID od the sender.
+     */
+    void punishReservation(long reservationId, String accountSender);
 
+    /**
+     * Get the amount of all money on the account. Reserved money is still included here.
+     *
+     * @param account accountId.
+     * @return account balance.
+     */
     int getAccountBalance(String account);
 
-    void addMoney(String account, int amount);
-
-    void transferMoney(int amount, String fromAccount, String toAccount);
-
+    /**
+     * Get the sum of all money reserved on the account.
+     *
+     * @param account accountId.
+     * @return money reserved.
+     */
     int getAccountReservations(String account);
 
+    /**
+     * Get the amount of all money on the account the user has access to. Reservations
+     * are not included here.
+     *
+     * @param account accountId.
+     * @return account balance.
+     */
     int getAccountBalanceLiquid(String account);
+
+    /**
+     * Add money to the account. Money comes from nothing...
+     * (Mostly used for test purposes).
+     *
+     * @param account accountId.
+     * @param amount  amount of money.
+     */
+    void addMoney(String account, int amount);
+
+    /**
+     * Transfer money from one account to another.
+     *
+     * @param amount      amount transferred.
+     * @param fromAccount account sender.
+     * @param toAccount   account recipiant.
+     */
+    void transferMoney(int amount, String fromAccount, String toAccount);
 }
