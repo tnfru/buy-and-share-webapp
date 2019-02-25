407d7e562e098a3a2b37a533b875571dd46a737d
Philipp Krause <phil.fhs11@googlemail.com>
2019-02-25 14:04:07 +0100

    FIX: checkstyle

---
+++ src/main/java/de/hhu/propra/sharingplatform/service/ApiService.java
@@ -0,0 +1,83 @@
+package de.hhu.propra.sharingplatform.service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+public class ApiService {
+
+    private static final String host = "localhost";
+
+    public static String buildRequest(String requestType, String serverAddress,
+        List<String> pathVars,
+        Map<String, String> parameters) {
+        StringBuilder urlBuilder = new StringBuilder(serverAddress);
+        // append path variables
+        for (String pathVar : pathVars) {
+            urlBuilder.append(pathVar + "/");
+        }
+        urlBuilder.deleteCharAt(urlBuilder.lastIndexOf("/"));
+        // append parameters
+        urlBuilder.append("?");
+        for (String parameter : parameters.keySet()) {
+            urlBuilder.append(parameter + "=" + parameters.get(parameter) + "&");
+        }
+        urlBuilder.deleteCharAt(urlBuilder.lastIndexOf("&"));
+        URL url;
+        try {
+            url = new URL(urlBuilder.toString());
+        } catch (MalformedURLException malFormedException) {
+            malFormedException.printStackTrace();
+            return "-1";
+        }
+        try {
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod(requestType);
+            conn.setDoOutput(true);
+            return convertHttpResponse(new InputStreamReader(conn.getInputStream()));
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+            return "-1";
+        }
+    }
+
+    public static String convertHttpResponse(InputStreamReader inStream) {
+        try {
+            BufferedReader in = new BufferedReader(inStream);
+            String input;
+            StringBuilder inBuffer = new StringBuilder();
+            while ((input = in.readLine()) != null) {
+                inBuffer.append(input);
+            }
+            in.close();
+            return inBuffer.toString();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String fetchJson(String userName) {
+        String url = "http://" + host + ":8888/account/" + userName;
+        RestTemplate jsonResponse = new RestTemplate();
+
+        String response;
+        try {
+            response = jsonResponse.getForObject(url, String.class);
+        } catch (Exception exception) {
+            throw new ResponseStatusException(HttpStatus.REQUEST_TIMEOUT,
+                "Couldnt reach Propayserver!");
+        }
+        return response;
+    }
+}
