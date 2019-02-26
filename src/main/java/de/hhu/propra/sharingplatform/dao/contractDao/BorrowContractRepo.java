f083003ed6bd357e61f83ef8b2d2d6f0e83e941f
Jonathan Bobak <jonathan.bobak@yahoo.de>
2019-02-26 11:35:16 +0100

    ADD: dao for all contract entities

---
+++ src/main/java/de/hhu/propra/sharingplatform/dao/contractDao/BorrowContractRepo.java
@@ -0,0 +1,19 @@
+package de.hhu.propra.sharingplatform.dao.contractDao;
+
+import de.hhu.propra.sharingplatform.model.Item;
+import de.hhu.propra.sharingplatform.model.contracts.BorrowContract;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface BorrowContractRepo extends CrudRepository<BorrowContract, Long> {
+
+    BorrowContract findOneById(long id);
+
+    List<BorrowContract> findAllByItem(Item item);
+
+    List<BorrowContract> findAllByItemAndFinishedIsFalse(Item item);
+
+    List<BorrowContract> findAll();
+
+}
---
+++ src/main/java/de/hhu/propra/sharingplatform/dao/contractDao/ContractRepo.java
@@ -0,0 +1,16 @@
+package de.hhu.propra.sharingplatform.dao.contractDao;
+
+import de.hhu.propra.sharingplatform.model.Item;
+import de.hhu.propra.sharingplatform.model.contracts.Contract;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ContractRepo extends CrudRepository<Contract, Long> {
+
+    Contract findOneById(long id);
+
+    List<Contract> findAllByItem(Item item);
+
+    List<Contract> findAllByItemAndFinishedIsFalse(Item item);
+}
