8250b05017ee31fba4a67bd875e3efa314d581a0
Philipp Krause <phil.fhs11@googlemail.com>
2019-02-26 15:22:30 +0100

    ADD: itemSale class

--- src/main/java/de/hhu/propra/sharingplatform/model/Item.java
+++ src/main/java/de/hhu/propra/sharingplatform/model/Item.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.ManyToOne;
 import javax.persistence.Transient;
 import lombok.Data;
@@ -16,10 +18,11 @@ import org.springframework.web.multipart.MultipartFile;
 @Data
 @Entity
 @ToString(exclude = "owner")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Item {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
