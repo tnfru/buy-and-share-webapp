2097a7b4269db5402cc6840817e16a5e145ede8a
Stefan Matz <matz.stefan1@gmail.com>
2019-02-26 21:26:59 +0100

    UPDATE: set application-dev.properties for SpringBootTests

--- src/test/java/de/hhu/propra/sharingplatform/dao/ConflictRepoTest.java
+++ src/test/java/de/hhu/propra/sharingplatform/dao/ConflictRepoTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -16,6 +17,7 @@ import static org.junit.Assert.assertEquals;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@TestPropertySource(locations = "classpath:application-dev.properties")
 public class ConflictRepoTest {
 
     @Autowired
