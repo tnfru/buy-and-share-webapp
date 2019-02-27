ad5f9d0b5f96c96331943e72351bf28e021d2f6e
Ingo Kral <ingo.kral@hhu.de>
2019-02-27 10:07:14 +0100

    FIX: Now all Users get propper Conflict messages

--- src/main/java/de/hhu/propra/sharingplatform/controller/BaseController.java
+++ src/main/java/de/hhu/propra/sharingplatform/controller/BaseController.java
@@ -1,21 +1,18 @@
 package de.hhu.propra.sharingplatform.controller;
 
-import de.hhu.propra.sharingplatform.dao.UserRepo;
 import de.hhu.propra.sharingplatform.model.Conflict;
 import de.hhu.propra.sharingplatform.model.Contract;
+import de.hhu.propra.sharingplatform.model.Item;
 import de.hhu.propra.sharingplatform.model.User;
 import de.hhu.propra.sharingplatform.service.UserService;
-import org.hibernate.validator.internal.util.Contracts;
+import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.ui.Model;
 
-import javax.swing.text.html.Option;
 import java.security.Principal;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 
 @Controller
@@ -33,21 +30,29 @@ public class BaseController {
 
         String message = "";
         if (principal != null) {
+            message = generateOverdueMessage(principal, message);
+        }
+        return message;
+    }
 
-            List<Contract> contracts = userService.fetchUserByAccountName(principal.getName())
-                .getChosenContracts(false);
-            LocalDateTime now = LocalDateTime.now();
+    private String generateOverdueMessage(Principal principal, String message) {
+        List<Contract> contracts = userService.fetchUserByAccountName(principal.getName())
+            .getChosenContracts(false);
+        LocalDateTime now = LocalDateTime.now();
 
-            for (Contract contract : contracts) {
-                if (contract.getRealEnd() == null) {
-                    if (now.isAfter(contract.getExpectedEnd())) {
-                        message += "Your Contract with User "
-                            + contract.getItem().getOwner().getAccountName()
-                            + " regarding Item "
-                            + contract.getItem().getName()
-                            + " is overdue \n";
-                    }
-                }
+        message = writeOverdueMessage(message, contracts, now);
+        return message;
+    }
+
+    private String writeOverdueMessage(String message, List<Contract> contracts,
+        LocalDateTime now) {
+        for (Contract contract : contracts) {
+            if (contract.getRealEnd() == null && now.isAfter(contract.getExpectedEnd())) {
+                message += "Your Contract with User "
+                    + contract.getItem().getOwner().getAccountName()
+                    + " regarding Item "
+                    + contract.getItem().getName()
+                    + " is overdue \n";
             }
         }
         return message;
@@ -58,26 +63,33 @@ public class BaseController {
 
         String message = "";
         if (principal != null) {
+            message = generateConflictMessage(principal, message);
+        }
+        return message;
+    }
 
-            User user = userService.fetchUserByAccountName(principal.getName());
-            List<Contract> contracts = user.getChosenContracts(false);
-
-
-            for (Contract contract : contracts) {
-                List<Conflict> conflicts = contract.getOpenConflicts();
-                if (!conflicts.isEmpty()) {
-                    if (user != conflicts.get(0).getRequester()) {
-                        message += "You have a conflict regarding Item "
-                            + contract.getItem().getName()
-                            + "\n";
-                    }
-                }
-
-            }
+    private String generateConflictMessage(Principal principal, String message) {
+        User user = userService.fetchUserByAccountName(principal.getName());
+        Collection<Contract> contracts = user.getChosenContracts(false);
+        Collection<Item> items = user.getItemsExcludeRemoved();
+        for (Item item : items) {
+            contracts.addAll(item.getChosenContracts(false));
         }
 
+        message = writeConflictMessage(message, user, contracts);
         return message;
+    }
 
+    private String writeConflictMessage(String message, User user, Collection<Contract> contracts) {
+        for (Contract contract : contracts) {
+            List<Conflict> conflicts = contract.getOpenConflicts();
+            if (!conflicts.isEmpty() && user != conflicts.get(0).getRequester()) {
+                message += "You have a conflict regarding Item "
+                    + contract.getItem().getName()
+                    + "\n";
+            }
+        }
+        return message;
     }
 
 
