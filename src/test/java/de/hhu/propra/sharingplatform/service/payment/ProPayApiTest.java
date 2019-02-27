0ef55a2a002f5ca4383b96b53a6845a3bfc9ed61
Christian Laußmann <ch.laussmann@gmail.com>
2019-02-27 12:27:42 +0100

    ADD: Tests

--- src/test/java/de/hhu/propra/sharingplatform/service/payment/ProPayApiTest.java
+++ src/test/java/de/hhu/propra/sharingplatform/service/payment/ProPayApiTest.java
@@ -1,14 +1,63 @@
 package de.hhu.propra.sharingplatform.service.payment;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class ProPayApiTest {
 
-    @Before
-    public void setup(){
+    ProPayApi api;
+    ProPayNetworkInterface networkInterface;
 
+    @Before
+    public void setup() {
+        api = new ProPayApi();
+        api.host = "google";
+        networkInterface = mock(ProPayNetworkInterface.class);
+        api.networkInterface = networkInterface;
+    }
+
+    @Test
+    public void addMoney() {
+        List<String> pathVariables = new ArrayList<>();
+        pathVariables.add("account");
+        pathVariables.add("foo");
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("amount", "1");
+
+        api.addMoney("foo", 1);
+
+        verify(networkInterface).buildRequest("POST",
+            "http://google:8888/", pathVariables, parameters);
+    }
+
+    @Test
+    public void reserve() {
+        List<String> pathVariables = new ArrayList<>();
+        pathVariables.add("reservation");
+        pathVariables.add("reserve");
+        pathVariables.add("foo");
+        pathVariables.add("bar");
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("amount", "1");
+        when(networkInterface.buildRequest(any(), any(), anyList(), anyMap()))
+            .thenReturn("{\n" +
+                "  \"amount\": 0,\n" +
+                "  \"id\": 0\n" +
+                "}");
+
+        long id = api.reserveMoney("foo", "bar", 1);
+
+        verify(networkInterface).buildRequest("POST",
+            "http://google:8888/", pathVariables, parameters);
+        assertEquals(id, 0);
     }
 
 }
